53c796896e4068589dd7be7783b71f886b71a4c5
roksid20-dev <roksid20@gmail.com>
2026-07-23 17:05:10 +0300

    Create algorithm.js

---
+++ v2/js/algorithm.js
@@ -0,0 +1,229 @@
+/*
+WordFlow
+algorithm.js
+
+Логика выбора слов для тренировки
+*/
+
+
+let lastWordId = null;
+
+
+
+let lastDirection = null;
+
+
+
+
+/**
+ * Выбирает слова для умной тренировки
+ */
+function getSmartWords(words) {
+
+
+    const now =
+        new Date();
+
+
+
+    return words
+        .filter(word => {
+
+
+            const reviewDate =
+                new Date(
+                    word.nextReview
+                );
+
+
+            return reviewDate <= now;
+
+
+        })
+        .sort((a,b)=>{
+
+
+            return (
+                calculatePriority(b)
+                -
+                calculatePriority(a)
+            );
+
+
+        });
+
+
+}
+
+
+
+
+
+/**
+ * Свободная тренировка
+ *
+ * Все слова,
+ * но перемешанные
+ */
+function getFreeWords(words){
+
+
+    return [...words]
+        .sort(
+            ()=>Math.random()-0.5
+        );
+
+
+}
+
+
+
+
+
+/**
+ * Приоритет слова
+ */
+function calculatePriority(word){
+
+
+    let priority = 0;
+
+
+
+    /*
+    Чем ниже уровень,
+    тем чаще повторяем
+    */
+
+    priority +=
+        (5 - word.level);
+
+
+
+    /*
+    Ошибки увеличивают важность
+    */
+
+    priority +=
+        word.stats.enRu.wrong +
+        word.stats.ruEn.wrong;
+
+
+
+    return priority;
+
+
+}
+
+
+
+
+
+/**
+ * Выбор направления
+ *
+ * en-ru
+ * ru-en
+ */
+function chooseDirection(word){
+
+
+    const enRuMistakes =
+        word.stats.enRu.wrong;
+
+
+    const ruEnMistakes =
+        word.stats.ruEn.wrong;
+
+
+
+    if(
+        enRuMistakes >
+        ruEnMistakes
+    ){
+
+        return "en-ru";
+
+    }
+
+
+
+    if(
+        ruEnMistakes >
+        enRuMistakes
+    ){
+
+        return "ru-en";
+
+    }
+
+
+
+    /*
+    если одинаково —
+    случайно
+    */
+
+    return Math.random() < 0.5
+        ? "en-ru"
+        : "ru-en";
+
+
+}
+
+
+
+
+
+/**
+ * Берём следующее слово
+ */
+function getNextWord(words){
+
+
+    if(words.length === 0){
+
+        return null;
+
+    }
+
+
+
+    let available =
+        words.filter(
+            word =>
+            word.id !== lastWordId
+        );
+
+
+
+    if(
+        available.length === 0
+    ){
+
+        available = words;
+
+    }
+
+
+
+    const word =
+        available[
+            Math.floor(
+                Math.random()
+                *
+                available.length
+            )
+        ];
+
+
+
+    lastWordId =
+        word.id;
+
+
+
+    return word;
+
+
+}
