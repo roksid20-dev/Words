90f1717a838f394c6107c00ca3d7b7e856afab43
roksid20-dev <roksid20@gmail.com>
2026-07-23 16:50:16 +0300

    Update app.js

--- v2/js/app.js
+++ v2/js/app.js
@@ -1,1 +1,54 @@
-console.log("WordFlow v2");
+/*
+WordFlow
+app.js
+
+Главная точка запуска приложения.
+*/
+
+
+console.log(
+    "WordFlow запущен"
+);
+
+
+/*
+Проверяем,
+что основные модули подключены
+*/
+
+
+if(
+    typeof loadWords !== "function" ||
+    typeof createWord !== "function"
+){
+
+    console.error(
+        "Ошибка: модули WordFlow не загружены"
+    );
+
+}
+
+
+
+/*
+Первичная инициализация
+*/
+
+
+function initApp(){
+
+
+    console.log(
+        "Слов в базе:",
+        words.length
+    );
+
+
+    renderWords();
+
+
+}
+
+
+
+initApp();
