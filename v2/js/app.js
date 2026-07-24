5811290e6ce0730de84f1027a91dfc6e8c0869bc
roksid20-dev <roksid20@gmail.com>
2026-07-24 04:17:01 +0300

    Update app.js

--- v2/js/app.js
+++ v2/js/app.js
@@ -52,3 +52,88 @@ function initApp(){
 
 
 initApp();
+
+function updateWordsStats(){
+
+    let learned = 0;
+    let learning = 0;
+    let newWords = 0;
+
+
+    words.forEach(
+        word => {
+
+
+            const enRu =
+                word.review.enRu.level;
+
+
+            const ruEn =
+                word.review.ruEn.level;
+
+
+
+            if(
+                enRu >= 5 &&
+                ruEn >= 5
+            ){
+
+                learned++;
+
+            }
+            else if(
+                enRu > 0 ||
+                ruEn > 0
+            ){
+
+                learning++;
+
+            }
+            else {
+
+                newWords++;
+
+            }
+
+
+        }
+    );
+
+
+
+    document
+        .getElementById(
+            "totalWords"
+        )
+        .textContent =
+            words.length;
+
+
+
+    document
+        .getElementById(
+            "learnedWords"
+        )
+        .textContent =
+            learned;
+
+
+
+    document
+        .getElementById(
+            "learningWords"
+        )
+        .textContent =
+            learning;
+
+
+
+    document
+        .getElementById(
+            "newWords"
+        )
+        .textContent =
+            newWords;
+
+
+}
